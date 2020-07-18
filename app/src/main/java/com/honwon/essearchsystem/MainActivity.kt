d9ee75805e8612bf2dc6bf06caaf9f29fc5a584f
honwon <moheom98@gmail.com>
2020-07-19 04:13:05 +0900

    Initial commit

---
+++ app/src/main/java/com/honwon/essearchsystem/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.honwon.essearchsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
